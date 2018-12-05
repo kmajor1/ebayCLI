4240091c9d1cc5480be67e230a08f9c90cf8fb9a
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-05 00:30:19 -0500

    added process env file

--- query.js
+++ query.js
@@ -1,16 +1,21 @@
 var mysql = require('mysql');
+// to use env files 
+require('dotenv').config(); 
+// set password const
+const pw = process.env.password; 
 
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     port: 3306,
-    password: 'Jadsf1234!@#$',
+    password: pw,
     database: 'great_bay_db'
 });
 
 // function if requested action is to post 
 function postQuery() {
     connection.connect();
+    // will need to create a function to dynamically construct the query string 
     connection.query('INSERT into item_table (item_name, item_msrp, highest_bid, item_description) values("ice cream", 4, 0,"slightly used")', 
     function (error, results) {
         if (error) {
