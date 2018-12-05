f4ca215a509041988e929b59c1c2d648176eed41
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-04 20:50:07 -0500

    post or bid message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const cli = require('prompt');
+const cli = require('inquirer'); 
+
+let postOrBid = [
+    {
+        type: 'list',
+        name: 'postOrBid',
+        message: 'Would you like to Post an ad or Bid on an item?',
+        choices: ['Post','Bid']
+    }
+]
+
+cli.prompt(postOrBid)
+    .then(function(response) {
+        console.log(response);
+    })
