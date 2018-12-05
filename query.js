9a1519f7b835bdaebe0484d8ccc0ccd9ea59aca4
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-05 00:25:47 -0500

    moved query process flow into two functions for query

--- query.js
+++ query.js
@@ -8,27 +8,30 @@ var connection = mysql.createConnection({
     database: 'great_bay_db'
 });
 
-var input = process.argv[2]
-
-if (input == 'post') {
+// function if requested action is to post 
+function postQuery() {
     connection.connect();
+    connection.query('INSERT into item_table (item_name, item_msrp, highest_bid, item_description) values("ice cream", 4, 0,"slightly used")', 
+    function (error, results) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(results);
+        connection.end()
+    });
+}
+
+function bidQuery() {
     var bid = 'SELECT * FROM great_bay_db.item_table'
-
-
     connection.query(bid, function (error, results) {
         if (error) {
             console.log(error);
         }
         console.log(results)
-        connection.query('INSERT into item_table (item_name, item_msrp, highest_bid, item_description) values("ice cream", 4, 0,"slightly used")', function (error, results) {
-            if (error){
-                console.log(error);
-            }
-        console.log(results);
-        connection.end()
-        });
-});
+    });
 }
+
+
 
 
 
